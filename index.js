4f43a01d2f17002a0e0f39e776558e1818c44ebf
maciek <macieq2048@gmail.com>
2022-01-17 16:50:29 +0100

    initial

---
+++ index.js
@@ -0,0 +1,63 @@
+const { Client, Intents } = require('discord.js');
+
+const TOKEN = "OTEzODI4NjE2MTA0NzM0NzYw.YaELOA.vxuVsYpgjTGdN4HQkKpuR_LSMbE";
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const messageFilter = (msg) => {
+	if (msg.author.id == client.user.id) return false;
+
+	return msg.content.toLowerCase().includes("piw");
+}
+
+let collectors = [];
+
+const reloadCollectors = () => {
+	console.log("Reloadign collectors ...");
+	client.guilds.fetch()
+		.then((partialguilds) =>
+			Promise.all(partialguilds.map((partialguild) => {
+				return partialguild.fetch();
+			}))
+		)
+		.then((guilds) =>
+			Promise.all(guilds.map((guild) => {
+				return guild.channels.fetch();
+			}))
+		)
+		.then((listofchannels) => {
+			// channels is list of maps, figure that out
+			return listofchannels.flatMap((channels) => {
+				return Array.from(channels)
+					.filter(([_, channel]) => channel.isText())
+					.map(([_, channel]) => channel);
+			});
+		})
+		.then((channels) => {
+			// Close all opened collectors
+			collectors.forEach((collector) => { collector.stop(); });
+			collectors = [];
+			channels.forEach((channel) => {
+				const filter = messageFilter;
+				const collector = channel.createMessageCollector({ filter});
+				collector.on('collect', (msg) => {
+					channel.send("Czy ktoś powiedział piwo ?");
+					msg.react("🍻");
+				});
+
+				collectors.push(collector);
+			});
+
+			console.log(`Loaded ${collectors.length} collectors !`);
+		});
+}
+
+// When the client is ready, run this code (only once)
+client.once('ready', async () => {
+	setImmediate(reloadCollectors);
+	setInterval(reloadCollectors, 1000 * 60 * 20);
+});
+
+// Login to Discord with your client's token
+client.login(TOKEN);
